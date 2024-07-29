95458a69459cf6f1a54e5cdd0975f0bede45cbc3
DevOpsTrainerNew <gcptrainer4@gmail.com>
2024-07-29 09:51:19 +0530

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 def add(a, b):
     return a + b
 
@@ -9,11 +10,9 @@ class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add(1, 2), 3)
 
-    
     def test_add_negative_numbers(self):
         self.assertEqual(add(-1, -2), -3)
 
-    
     def test_add_mixed_numbers(self):
         self.assertEqual(add(1, -2), -1)
         self.assertEqual(add(-1, 2), 1)
